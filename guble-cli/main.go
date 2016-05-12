f198b447343c54929243d2b85659d61d358a2ba6
Marian Craciunescu <m.craciunescu@tarent.de>
2016-05-12 17:39:13 +0300

    added small test for removeTrailingSlash

--- guble-cli/main.go
+++ guble-cli/main.go
@@ -149,7 +149,7 @@ func printHelp() {
 }
 
 func removeTrailingSlash(path string) string {
-	if len(path) > 0 && path[len(path)-1] == '/' {
+	if len(path) > 1 && path[len(path)-1] == '/' {
 		return path[:len(path)-1]
 	}
 	return path
---
+++ guble-cli/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+)
+
+func Test_removeTrailingSlash(t *testing.T) {
+	cases := []struct {
+		expected, path string
+	}{
+		{"/foo/user/marvin", "/foo/user/marvin"},
+		{"/foo/user/marvin", "/foo/user/marvin/"},
+		{"/", "/"},
+	}
+
+	for i, c := range cases {
+		assert.Equal(t, c.expected, removeTrailingSlash(c.path), fmt.Sprintf("Failed at  case no=%d", i))
+	}
+}
+
