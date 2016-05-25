cc531557e391e634612617157a7520adfb05d47c
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2016-05-25 11:18:32 +0300

    Simple covearage script

--- scripts/generate_coverage.sh
+++ scripts/generate_coverage.sh
@@ -6,14 +6,13 @@ cd $GOPATH/src/github.com/smancke/guble
 rm -rf ./cov
 mkdir cov
 
-i=0
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_test.go' -type d);
-do
-if ls $dir/*.go &> /dev/null; then
-    go test -v -covermode=atomic -coverprofile=./cov/$i.out ./$dir
-    i=$((i+1))
-fi
-done
+go test -v -covermode=atomic -coverprofile=./cov/client.out ./client
+go test -v -covermode=atomic -coverprofile=./cov/gcm.out ./gcm
+go test -v -covermode=atomic -coverprofile=./cov/guble-cli.out ./guble-cli
+go test -v -covermode=atomic -coverprofile=./cov/gubled.out ./gubled
+go test -v -covermode=atomic -coverprofile=./cov/protocol.out ./protocol
+go test -v -covermode=atomic -coverprofile=./cov/server.out ./server
+go test -v -covermode=atomic -coverprofile=./cov/store.out ./store
 
 gocovmerge ./cov/*.out > full_cov.out
 rm -rf ./cov
