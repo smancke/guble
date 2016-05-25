d726187de76a2e00621e2b31ad60da5300840eed
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2016-05-25 11:02:59 +0300

    Add coverage back to `travis.yml`

--- scripts/generate_coverage.sh
+++ scripts/generate_coverage.sh
@@ -2,11 +2,13 @@
 # Run from directory above via ./scripts/cov.sh
 # Require installation of: `github.com/wadey/gocovmerge`
 
+cd $GOPATH/src/github.com/smancke/guble
+
 rm -rf ./cov
 mkdir cov
 
 i=0
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_test.go' -type d);
 do
 if ls $dir/*.go &> /dev/null; then
     go test -v -covermode=atomic -coverprofile=./cov/$i.out ./$dir
