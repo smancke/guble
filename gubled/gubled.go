690d8196fefaa5aa91b069c784991ce4f3688b38
Marian Craciunescu <m.craciunescu@tarent.de>
2016-06-09 14:56:27 +0300

    Added logrus for gubled

--- gubled/gubled.go
+++ gubled/gubled.go
@@ -1,6 +1,8 @@
 package gubled
 
 import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/alexflint/go-arg"
 	"github.com/caarlos0/env"
 	"github.com/smancke/guble/gcm"
@@ -11,8 +13,6 @@ import (
 	"github.com/smancke/guble/server/webserver"
 	"github.com/smancke/guble/server/websocket"
 	"github.com/smancke/guble/store"
-
-	"fmt"
 	"os"
 	"os/signal"
 	"path"
@@ -37,9 +37,18 @@ var ValidateStoragePath = func(args Args) error {
 		testfile := path.Join(args.StoragePath, "write-test-file")
 		f, err := os.Create(testfile)
 		if err != nil {
-			protocol.ErrWithoutTrace("Storage path not present/writeable %q: %v", args.StoragePath, err)
+			log.WithFields(log.Fields{
+				"module":       "gubled",
+				"storage_Path": args.StoragePath,
+				"err":          err,
+			}).Error("Storage path not present/writeable.")
+
 			if args.StoragePath == "/var/lib/guble" {
-				protocol.ErrWithoutTrace("Use --storage-path=<path> to override the default location, or create the directory with RW rights.")
+				log.WithFields(log.Fields{
+					"module":       "gubled",
+					"storage_Path": args.StoragePath,
+					"err":          err,
+				}).Error("Use --storage-path=<path> to override the default location, or create the directory with RW rights.")
 			}
 			return err
 		}
@@ -56,11 +65,15 @@ var CreateKVStore = func(args Args) store.KVStore {
 	case "file":
 		db := store.NewSqliteKVStore(path.Join(args.StoragePath, "kv-store.db"), true)
 		if err := db.Open(); err != nil {
-			panic(err)
+			log.WithFields(log.Fields{
+				"module": "gubled",
+				"err":    err,
+			}).Panic("Could not open db connection")
+
 		}
 		return db
 	default:
-		panic(fmt.Errorf("unknown key-value backend: %q", args.KVBackend))
+		panic(fmt.Errorf("Unknown key-value backend: %q", args.KVBackend))
 	}
 }
 
@@ -69,10 +82,13 @@ var CreateMessageStore = func(args Args) store.MessageStore {
 	case "none", "":
 		return store.NewDummyMessageStore(store.NewMemoryKVStore())
 	case "file":
-		protocol.Info("using FileMessageStore in directory: %q", args.StoragePath)
+		log.WithFields(log.Fields{
+			"module":       "gubled",
+			"storage_Path": args.StoragePath,
+		}).Info("Using FileMessageStore in directory")
 		return store.NewFileMessageStore(args.StoragePath)
 	default:
-		panic(fmt.Errorf("unknown message-store backend: %q", args.MSBackend))
+		panic(fmt.Errorf("Unknown message-store backend: %q", args.MSBackend))
 	}
 }
 
@@ -80,7 +96,10 @@ var CreateModules = func(router server.Router, args Args) []interface{} {
 	modules := make([]interface{}, 0, 3)
 
 	if wsHandler, err := websocket.NewWSHandler(router, "/stream/"); err != nil {
-		protocol.Err("Error loading WSHandler module: %s", err)
+		log.WithFields(log.Fields{
+			"module": "gubled",
+			"err":    err,
+		}).Error("Error loading WSHandler module:")
 	} else {
 		modules = append(modules, wsHandler)
 	}
@@ -89,17 +108,33 @@ var CreateModules = func(router server.Router, args Args) []interface{} {
 
 	if args.GcmEnable {
 		if args.GcmApiKey == "" {
-			panic("GCM API Key has to be provided, if GCM is enabled")
+			log.WithFields(log.Fields{
+				"module": "gubled",
+			}).Panic("GCM API Key has to be provided, if GCM is enabled")
+
 		}
-		protocol.Info("google cloud messaging: enabled")
-		protocol.Debug("gcm: %v workers", args.GcmWorkers)
+
+		log.WithFields(log.Fields{
+			"module": "gubled",
+		}).Info("Google cloud messaging: enabled")
+
+		log.WithFields(log.Fields{
+			"module":          "gubled",
+			"args.GcmWOrkers": args.GcmWorkers,
+		}).Debug("Workers")
+
 		if gcm, err := gcm.NewGCMConnector(router, "/gcm/", args.GcmApiKey, args.GcmWorkers); err != nil {
-			protocol.Err("Error loading GCMConnector: ", err)
+			log.WithFields(log.Fields{
+				"module": "gubled",
+				"err":    err,
+			}).Error("Error loading GCMConnector:")
 		} else {
 			modules = append(modules, gcm)
 		}
 	} else {
-		protocol.Info("google cloud messaging: disabled")
+		log.WithFields(log.Fields{
+			"module": "gubled",
+		}).Info("Google cloud messaging: disabled")
 	}
 
 	return modules
@@ -108,21 +143,27 @@ var CreateModules = func(router server.Router, args Args) []interface{} {
 func Main() {
 	defer func() {
 		if p := recover(); p != nil {
-			protocol.Err("%v", p)
-			os.Exit(1)
+			log.WithFields(log.Fields{
+				"module": "gubled",
+				"err":    p,
+			}).Fatal("Fatal error in gubled after recover")
 		}
 	}()
 
 	args := loadArgs()
+	args = loadArgs()
 	if args.LogInfo {
-		protocol.LogLevel = protocol.LEVEL_INFO
+		log.SetLevel(log.InfoLevel)
 	}
 	if args.LogDebug {
-		protocol.LogLevel = protocol.LEVEL_DEBUG
+		log.SetLevel(log.DebugLevel)
 	}
 
 	if err := ValidateStoragePath(args); err != nil {
-		os.Exit(1)
+		log.WithFields(log.Fields{
+			"module": "gubled",
+			"err":    err,
+		}).Fatal("Fatal error in gubled in validation for storage path")
 	}
 
 	service := StartService(args)
@@ -130,7 +171,10 @@ func Main() {
 	waitForTermination(func() {
 		err := service.Stop()
 		if err != nil {
-			protocol.Err("service: error when stopping: ", err)
+			log.WithFields(log.Fields{
+				"module": "gubled",
+				"err":    err,
+			}).Error("Error when stopping service")
 		}
 	})
 }
@@ -150,9 +194,15 @@ func StartService(args Args) *server.Service {
 	if err := service.Start(); err != nil {
 		protocol.Err(err.Error())
 		if err := service.Stop(); err != nil {
-			protocol.Err(err.Error())
+			log.WithFields(log.Fields{
+				"module": "gubled",
+				"err":    err,
+			}).Error("Error when stopping service after Start() failed")
 		}
-		os.Exit(1)
+		log.WithFields(log.Fields{
+			"module": "gubled",
+			"err":    err,
+		}).Fatal("Service could not be started")
 	}
 
 	return service
@@ -175,8 +225,14 @@ func loadArgs() Args {
 func waitForTermination(callback func()) {
 	signalC := make(chan os.Signal)
 	signal.Notify(signalC, syscall.SIGINT, syscall.SIGTERM)
-	protocol.Info("Got signal '%v' .. exiting gracefully now", <-signalC)
+
+	log.WithFields(log.Fields{
+		"module": "gubled",
+	}).Infof("Got signal '%v' .. exiting gracefully now", <-signalC)
+
 	callback()
-	protocol.Info("exit now")
+	log.WithFields(log.Fields{
+		"module": "gubled",
+	}).Info("Exit gracefully now")
 	os.Exit(0)
 }
