b48c2fd345df7e0bb897c777929aa37340522572
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2016-06-15 16:35:18 +0300

    Config options comments

--- gubled/config/config.go
+++ gubled/config/config.go
@@ -14,31 +14,38 @@ const (
 )
 
 var (
+	// Listen the address to bind the HTTP server to
 	Listen = kingpin.Flag("listen", "[Host:]Port the address to listen on (:8080)").
 		Default(":8080").
 		Short('l').
 		Envar("GUBLE_LISTEN").
 		String()
+	// StoragePath where to save the file system store
 	StoragePath = kingpin.Flag("storage-path", "The path for storing messages and key value data if 'file' is enabled (/var/lib/guble)").
 			Short('p').
 			Envar("GUBLE_STORAGE_PATH").
 			String()
+	// KVBackend sets the key-value storage format to use
 	KVBackend = kingpin.Flag("kv-backend", "The storage backend for the key value store to use: file|memory (file)").
 			Default("file").
 			Envar("GUBLE_KV_BACKEND").
 			String()
+	// MSBackend sets the message store format to use
 	MSBackend = kingpin.Flag("ms-backend", "The message storage backend : file|memory (file)").
 			Default("file").
 			Envar("GUBLE_MS_BACKEND").
 			String()
+	// Health sets the health endpoint to bind in the HTTP server
 	Health = kingpin.Flag("health", `The health endpoint (default: /_health; value for disabling it: "")`).
 		Default(healthEndpointPrefix).
 		Envar("GUBLE_HEALTH_ENDPOINT").
 		String()
+	// Metrics sets the metrics endpoint to bind in the HTTP server and return  metrics data
 	Metrics = kingpin.Flag("metrics", "The metrics endpoint (disabled by default; a possible value for enabling it: /_metrics )").
 		Envar("GUBLE_METRICS_ENDPOINT").
 		String()
 
+	// GCM settings related to activating the GCM connector module
 	GCM = struct {
 		Enabled *bool
 		APIKey  *string
@@ -56,9 +63,8 @@ var (
 			Int(),
 	}
 
-	Log = struct {
-		Info, Debug *bool
-	}{
+	// Log settings
+	Log = struct{ Info, Debug *bool }{
 		Info: kingpin.Flag("log-info", "Log on INFO level (false)").
 			Default("false").
 			Envar("GUBLE_LOG_INFO").
