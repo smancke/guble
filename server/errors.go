8c2e50bab5d4480f252d4a472ea5b1aaf1c79de3
Cosmin Rentea <c.rentea@tarent.de>
2016-06-21 14:39:30 +0300

    partial revert

--- server/errors.go
+++ server/errors.go
@@ -12,10 +12,12 @@ var (
 	// ErrServiceNotProvided is returned when the service required is not set.
 	ErrServiceNotProvided = errors.New("Service not provided.")
 
-	// ErrInvalidRoute is returned by the `Deliver` method of a `Route` when it has been closed due to slow processing
+	// ErrInvalidRoute is returned by the `Deliver` method of a `Route` when it has been closed
+	// due to slow processing
 	ErrInvalidRoute = errors.New("Route is invalid. Channel is closed.")
 
-	// ErrChannelFull is returned when trying to `Deliver` a message with a queue size of zero and the channel is full
+	// ErrChannelFull is returned when trying to `Deliver` a message with a queue size of zero
+	// and the channel is full
 	ErrChannelFull = errors.New("Route channel is full. Route is closed.")
 
 	// ErrQueueFull is returned when trying to `Deliver` a message in a full queued route
