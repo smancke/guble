d6bb9eb6430c532b7ce8bd80bca2893ac5bf9252
Cosmin Rentea <c.rentea@tarent.de>
2016-06-24 15:59:21 +0300

    shebang

--- scripts/dependencies_graph.sh
+++ scripts/dependencies_graph.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -e
 (echo "digraph G {"
 go list -f '{{range .Imports}}{{printf "\t%q -> %q;\n" $.ImportPath .}}{{end}}' $(go list -f '{{join .Deps " "}}' github.com/smancke/guble ) github.com/smancke/guble
 echo "}" ) | dot -Tsvg -o dependencies_graph.svg
