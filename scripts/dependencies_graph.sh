a6d692b2be667c8e8107371cb0683a25667c9d13
Cosmin Rentea <c.rentea@tarent.de>
2016-06-24 15:58:34 +0300

    adding script to generate dependency graph of guble

---
+++ scripts/dependencies_graph.sh
@@ -0,0 +1,3 @@
+(echo "digraph G {"
+go list -f '{{range .Imports}}{{printf "\t%q -> %q;\n" $.ImportPath .}}{{end}}' $(go list -f '{{join .Deps " "}}' github.com/smancke/guble ) github.com/smancke/guble
+echo "}" ) | dot -Tsvg -o dependencies_graph.svg
