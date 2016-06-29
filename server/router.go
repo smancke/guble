4f3e38872f07ac71a614b5fee334378439d3090b
Cosmin Rentea <c.rentea@tarent.de>
2016-06-29 15:29:46 +0300

    improved logging

--- server/router.go
+++ server/router.go
@@ -122,16 +122,12 @@ func (router *router) Stop() error {
 
 func (router *router) Check() error {
 	if router.accessManager == nil || router.messageStore == nil || router.kvStore == nil {
-		logger.WithFields(log.Fields{
-			"err": ErrServiceNotProvided,
-		}).Error("Some mandatory services are not provided")
+		logger.WithField("err", ErrServiceNotProvided).Error("Some mandatory services are not provided")
 		return ErrServiceNotProvided
 	}
 	err := router.messageStore.Check()
 	if err != nil {
-		logger.WithFields(log.Fields{
-			"err": err,
-		}).Error("MessageStore check failed")
+		logger.WithField("err", err).Error("MessageStore check failed")
 		return err
 	}
 	err = router.kvStore.Check()
