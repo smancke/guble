ea2dd9f79b5461c33f5f97512e01759f728c30f8
Cosmin Rentea <c.rentea@tarent.de>
2016-06-30 16:45:59 +0300

    adding assertion

--- store/postgres_config_test.go
+++ store/postgres_config_test.go
@@ -7,8 +7,12 @@ import (
 
 func TestPostgresConfig_String(t *testing.T) {
 	a := assert.New(t)
+	pc0 := PostgresConfig{}
+	a.Equal(pc0.String(), "")
+
 	pc1 := PostgresConfig{"key": "value"}
 	a.Equal(pc1.String(), "key=value")
+
 	pc2 := PostgresConfig{"key": "value", "password": "secret"}
 	s := pc2.String()
 	a.True(s == "key=value password=secret" || s == "password=secret key=value")
