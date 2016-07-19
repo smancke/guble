2c2b54262c2f0c8e0a6a80671f2adbd0ba72a81c
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2016-07-19 12:05:16 +0300

    Separate RouteOptions struct in own file

--- server/route.go
+++ server/route.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"runtime"
-	"strings"
 	"sync"
 	"time"
 
@@ -21,78 +20,6 @@ var (
 	errEmptyQueue = errors.New("Empty queue")
 	errTimeout    = errors.New("Channel sending timeout")
 )
-
-type RouteOptions struct {
-	Path protocol.Path
-
-	ChannelSize int
-
-	// QueueSize specify the size of the internal queue slice.
-	// How many items to hold before the channel is closed.
-	// If set to `0` then the queue will have no capacity and the messages are sent
-	// directly, without buffering
-	QueueSize int
-
-	// Timeout to define how long to wait for the message to be read on the channel
-	// if timeout is reached the route is closed
-	Timeout time.Duration
-
-	RouteParams
-}
-
-type RouteParams map[string]string
-
-func (rp *RouteParams) String() string {
-	s := make([]string, 0, len(*rp))
-	for k, v := range *rp {
-		s = append(s, fmt.Sprintf("%s: %s", k, v))
-	}
-	return strings.Join(s, " ")
-}
-
-// Equal verifies if the `receiver` params are the same as `other` params
-// the `keys` param specifies which keys to check in case the match has to be
-// done only on a separate set of keys and not all
-func (rp *RouteParams) Equal(other RouteParams, keys ...string) bool {
-	if len(keys) > 0 {
-		return rp.partialEqual(other, keys)
-
-	}
-
-	if len(*rp) != len(other) {
-		return false
-	}
-
-	for k, v := range *rp {
-		if v2, ok := other[k]; !ok {
-			return false
-		} else if v != v2 {
-			return false
-		}
-	}
-
-	return true
-}
-
-func (rp *RouteParams) partialEqual(other RouteParams, fields []string) bool {
-	for _, key := range fields {
-		if v, ok := other[key]; !ok {
-			return false
-		} else if v != (*rp)[key] {
-			return false
-		}
-	}
-
-	return true
-}
-
-func (rp *RouteParams) Get(key string) string {
-	return (*rp)[key]
-}
-
-func (rp *RouteParams) Set(key, value string) {
-	(*rp)[key] = value
-}
 
 // Route represents a topic for subscription that has a channel to receive messages.
 type Route struct {
