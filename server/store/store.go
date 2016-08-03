afbee6ce91cf091780562cae19b7381f3064619d
Cosmin Rentea <c.rentea@tarent.de>
2016-08-03 16:05:32 +0300

    refactoring store package (unite the 2 files into a single one)

--- server/store/store.go
+++ server/store/store.go
@@ -32,3 +32,45 @@ type MessageStore interface {
 	// GenerateNextMsgId generates a new message ID based on a timestamp in a strictly monotonically order
 	GenerateNextMsgID(partition string, nodeID int) (uint64, int64, error)
 }
+
+// FetchRequest is used for fetching messages in a MessageStore.
+type FetchRequest struct {
+
+	// Partition is the Store name to search for messages
+	Partition string
+
+	// StartId is the message sequence id to start
+	StartID uint64
+
+	//EndId is the message sequence id to finish. If  will not be used.
+	EndID uint64
+
+	// Direction has 3 possible values:
+	// Direction == 0: Only the Message with StartId
+	// Direction == 1: Fetch also the next Count Messages with a higher MessageId
+	// Direction == -1: Fetch also the next Count Messages with a lower MessageId
+	Direction int
+
+	// Count is the maximum number of messages to return
+	Count int
+
+	// Prefix is the message prefix to filter
+	Prefix []byte
+
+	// MessageC is the channel to send the message back to the receiver
+	MessageC chan FetchedMessage
+
+	// ErrorC is a channel if an error occurs
+	ErrorC chan error
+
+	// StartC Through this channel , the total number or result
+	// is returned, before sending the first message.
+	// The Fetch() methods blocks on putting the number to the start channel.
+	StartC chan int
+}
+
+// FetchedMessage is a struct containing a pair: guble Message and its ID.
+type FetchedMessage struct {
+	ID      uint64
+	Message []byte
+}
