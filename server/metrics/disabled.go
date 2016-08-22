dcdc9b52eaf65c11b1023aa783b30bee967b32bf
Cosmin Rentea <c.rentea@tarent.de>
2016-08-22 17:52:07 +0300

    adding dummy Init implementation to disabled

--- server/metrics/disabled.go
+++ server/metrics/disabled.go
@@ -11,7 +11,6 @@ type dummyInt struct{}
 
 // Dummy functions on dummyInt
 func (v *dummyInt) Add(delta int64) {}
-
 func (v *dummyInt) Set(value int64) {}
 
 // NewInt returns a dummyInt, depending on the build tag declared at the beginning of this file.
@@ -22,11 +21,10 @@ func NewInt(name string) Int {
 type dummyMap struct{}
 
 // Dummy functions on dummyMap
-func (v *dummyMap) Get(key string) expvar.Var { return nil }
-
+func (v *dummyMap) Init() *expvar.Map             { return nil }
+func (v *dummyMap) Get(key string) expvar.Var     { return nil }
 func (v *dummyMap) Set(key string, av expvar.Var) {}
-
-func (v *dummyMap) Add(key string, delta int64) {}
+func (v *dummyMap) Add(key string, delta int64)   {}
 
 // NewMap returns a dummyMap, depending on the build tag declared at the beginning of this file.
 func NewMap(name string) Map {
