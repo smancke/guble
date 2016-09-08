73867174cb242d09eb4eb9d2061989d50c1dca23
Cosmin Rentea <c.rentea@tarent.de>
2016-09-08 17:09:40 +0300

    updating rest client interface and implementation (using generic query parameters)

--- restclient/sender.go
+++ restclient/sender.go
@@ -3,7 +3,7 @@ package restclient
 // Sender is an interface used to send a message to the guble server.
 type Sender interface {
 	// Send a a message(body) to the guble Server, to the given topic, with the given userID.
-	Send(topic string, body []byte, userID string) error
+	Send(topic string, body []byte, userID string, params map[string]string) error
 
 	// Check returns `true` if the guble server endpoint is reachable, or `false` otherwise.
 	Check() bool
