0c33987543f285aa8333b48184221ac1829b2156
Cosmin Rentea <c.rentea@tarent.de>
2016-11-04 12:44:37 +0200

    updating coverage scripts

--- scripts/generate_coverage.sh
+++ scripts/generate_coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# Requires installation of: `github.com/wadey/gocovmerge`
+# Requires local installation of: `github.com/wadey/gocovmerge`
 
 cd $GOPATH/src/github.com/smancke/guble
 
@@ -10,7 +10,7 @@ i=0
 for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_test.go' -type d);
 do
     if ls ${dir}/*.go &> /dev/null; then
-        go test -v -covermode=atomic -coverprofile=./cov/$i.out ./${dir}
+        GO_TEST_DISABLED=true go test -v -covermode=atomic -coverprofile=./cov/$i.out ./${dir}
         i=$((i+1))
     fi
 done
