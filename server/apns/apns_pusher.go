c9d34af7b2eee2e7209c0f504417d0caada76a1e
Michal Budzyn <michalbudzyn@gmail.com>
2017-02-10 18:06:47 +0100

    APNS2 client manager

--- server/apns/apns_pusher.go
+++ server/apns/apns_pusher.go
@@ -5,6 +5,15 @@ import (
 	"github.com/sideshow/apns2"
 	"github.com/sideshow/apns2/certificate"
 	"time"
+	"errors"
+)
+
+const (
+	useClientManager bool = false
+)
+
+var (
+	ErrNewClientCreation = errors.New("ClientManager cannot be create new APSN2 client")
 )
 
 type Pusher interface {
@@ -25,14 +34,51 @@ func newPusher(c Config) (Pusher, error) {
 		return nil, errCert
 	}
 
+	var clientFactory func(certificate tls.Certificate) *apns2.Client;
+	if *c.Production {
+		clientFactory = newProductionClient
+	} else {
+		clientFactory = newDevelopmentClient
+	}
+
 	//see https://github.com/sideshow/apns2/issues/24 and https://github.com/sideshow/apns2/issues/20
 	apns2.TLSDialTimeout = 40 * time.Second
 	apns2.HTTPClientTimeout = 60 * time.Second
 
-	if *c.Production {
-		logger.Debug("APNS Pusher in Production mode")
-		return apns2.NewClient(cert).Production(), nil
+	if useClientManager {
+		clientManager := apns2.NewClientManager()
+		clientManager.MaxSize = 4
+		clientManager.MaxAge = 1 * time.Minute
+		clientManager.Factory = clientFactory
+		mp := multiConnectionPusher {
+			cert: cert,
+			clientManager: clientManager,
+		}
+		return mp, nil
+
+	} else {
+		return clientFactory(cert), nil
 	}
-	logger.Debug("APNS Pusher in Development mode")
-	return apns2.NewClient(cert).Development(), nil
+}
+
+type multiConnectionPusher struct {
+	cert tls.Certificate
+	clientManager *apns2.ClientManager
+}
+
+func (mcp multiConnectionPusher) Push(n *apns2.Notification) (*apns2.Response, error) {
+	client := mcp.clientManager.Get(mcp.cert)
+	if client == nil {
+		return nil, ErrNewClientCreation
+	}
+	return client.Push(n)
+}
+
+func newProductionClient(certificate tls.Certificate) *apns2.Client {
+	logger.Info("APNS Pusher in Production mode")
+	return apns2.NewClient(certificate).Production()
+}
+func newDevelopmentClient(certificate tls.Certificate) *apns2.Client {
+	logger.Info("APNS Pusher in Development mode")
+	return apns2.NewClient(certificate).Development()
 }
